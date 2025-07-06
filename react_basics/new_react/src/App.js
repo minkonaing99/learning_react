5a227a6b7d9630d520c5fd0ed890d60bf649ea84
“minkonaing”99 <mkn.minko.naing99@gmail.com>
2025-07-06 22:57:40 +0700

    new changes

--- react_basics/new_react/src/App.js
+++ react_basics/new_react/src/App.js
@@ -1,6 +1,10 @@
 // import logo from "./logo.svg";
 import "./App.css";
-
-function App() {}
+function Heading() {
+  return <h1>This is heading for testing.</h1>;
+}
+function App() {
+  return <Heading />;
+}
 
 export default App;
